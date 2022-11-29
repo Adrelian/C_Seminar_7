231ecf9f62dbe1260ca7592c9cfad422f7eefa00
Anatolii <anatolii.polovnikov@gmail.com>
2022-11-29 22:41:54 +0500

    Программа для создания массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 48. Показать двумерный массив размером m×n заполненный целыми числами
+
+int [,] Creat2DRandomArray (int minValue, int maxValue, int rows, int columns)
+{
+    int [,] Matrix = new int[rows , columns]; // инициализация массива
+
+    for (int i = 0; i < rows; i++) // задание строк
+    {
+        for (int j = 0; j < columns; j++) // задание столбцов
+        {
+            Matrix [i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return Matrix;
+}
+
+void Show2DArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) // задание строк
+    {
+        for (int j = 0; j < array.GetLength(1); j++) // задание столбцов
+        {
+            Console.Write(array[i,j]+ " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Show2DArray(Creat2DRandomArray (0, 9, 4, 5));
